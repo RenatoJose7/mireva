16c132279cf2d08351992cef294ecebcec628e7d
Renato <renatojosesfc@gmail.com>
2026-04-27 09:42:44 -0300

    .

--- script.js
+++ script.js
@@ -38,3 +38,33 @@ prevCards.addEventListener("click", () => {
     behavior: "smooth"
   });
 });
+const scrollItems = document.querySelectorAll(".scroll-reveal");
+
+const showOnScroll = () => {
+  scrollItems.forEach((item) => {
+    const itemTop = item.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (itemTop < windowHeight - 100) {
+      item.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", showOnScroll);
+window.addEventListener("load", showOnScroll);
+
+const solutionCards = document.querySelectorAll(".solution-card");
+
+const showSolutionCards = () => {
+  solutionCards.forEach((card) => {
+    const cardTop = card.getBoundingClientRect().top;
+
+    if (cardTop < window.innerHeight - 100) {
+      card.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("scroll", showSolutionCards);
+window.addEventListener("load", showSolutionCards);
